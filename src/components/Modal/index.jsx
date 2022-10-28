33f28780c7069ee30309ceea538dbe2d2a79a099
Vinicius Pacheco <viniciusgrp@gmail.com>
2022-10-28 17:55:07 -0300

    add background color to modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,10 +5,17 @@ function Modal ({pokemon}) {
     const [frente, setFrente] = useState(true)
     console.log(pokemon)
 
+    function type(elem) {
+        return (
+            <h4>{elem.type.name}</h4>
+        )
+    }
+
     return(
-        <div className="pokemonAtual">
+        <div className={`pokemonAtual ${pokemon.types[0].type.name}`}>
             <h2>{pokemon.species.name}</h2>
             <img src={frente? pokemon.sprites.front_default : pokemon.sprites.back_default} alt="" />
+            {pokemon.types.map((elem) => type(elem))}
             <button onClick={() => setFrente(!frente)}><AiOutlineSwap/></button>
         </div>
     )
